e8de3ed90a44709abd3d35c608ac2d6f6b0a1631
William <wifryo@gmail.com>
2023-05-29 15:50:26 +0200

    Initialise project

---
+++ Readability Score (Java)/task/src/readability/Main.java
@@ -0,0 +1,12 @@
+package readability;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String inputText = scanner.next();
+        System.out.print(inputText);
+    }
+}
